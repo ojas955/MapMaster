517659c6844b1cf78a72e25b229286c5cf4c80ea
ansh-212 <anshathaleybs@gmail.com>
2026-02-22 11:33:47 +0530

    Add Railway deployment config

--- backend_final/main.py
+++ backend_final/main.py
@@ -56,10 +56,12 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:5173",
-        settings.FRONTEND_URL
+        origin for origin in [
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "http://127.0.0.1:5173",
+            settings.FRONTEND_URL,
+        ] if origin
     ],
     allow_credentials=True,
     allow_methods=["*"],
